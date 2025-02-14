f8bee1a341af20bc08db70a53fd37e274b1f5f38
Daniel Ritter <daniel.ritter@designr.ch>
2025-02-14 11:17:46 +0100

    Package.json creation works if no workspaces property in root package.json

--- packages/rollup/rollup.config.cjs
+++ packages/rollup/rollup.config.cjs
@@ -1,16 +1,17 @@
-const { withNx } = require('@nx/rollup/with-nx');
+const {withNx} = require('@nx/rollup/with-nx');
 
 module.exports = withNx(
-  {
-    main: './src/index.ts',
-    outputPath: './dist',
-    tsConfig: './tsconfig.lib.json',
-    compiler: 'swc',
-    format: ["esm"],
-  },
-  {
-    // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
-    // e.g.
-    // output: { sourcemap: true },
-  }
+    {
+        main: './src/index.ts',
+        outputPath: './dist',
+        tsConfig: './tsconfig.lib.json',
+        compiler: 'swc',
+        format: ["esm"],
+        generatePackageJson: true
+    },
+    {
+        // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
+        // e.g.
+        // output: { sourcemap: true },
+    }
 );
